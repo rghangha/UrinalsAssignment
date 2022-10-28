ba5709a9e5419f51e8182479da430cae0b4a7b19
rghangha <rghangha@asu.edu>
2022-10-27 21:46:23 -0700

    Added first function skeleton

--- Urinals/urinals.java
+++ Urinals/urinals.java
@@ -4,4 +4,8 @@ public class urinals {
     public static void main(String[] args){
         System.out.println("Author - Rahul Ghanghas");
     }
+    public static boolean goodString(String str){
+        System.out.println("Not yet implemented");
+        return true;
+    }
 }
