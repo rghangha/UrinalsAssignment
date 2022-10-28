ae53cc072ca908452928832d002758a4dd8737aa
rghangha <rghangha@asu.edu>
2022-10-27 22:18:06 -0700

    Function to count Urinals is added

--- Urinals/urinals.java
+++ Urinals/urinals.java
@@ -5,6 +5,12 @@ public class urinals {
         System.out.println("Author - Rahul Ghanghas");
         boolean validString = goodString("10001");
         System.out.println(validString);
+        int freeUrinals = countUrinals("10001");
+        System.out.println(freeUrinals);
+    }
+    public static int countUrinals(String str){
+        return str.contains("11") ? -1 : str.replaceAll("010|10|01","1")
+                .replace("00","0").replace("1","").length();
     }
     public static boolean goodString(String str){
         return str.matches("^[01]+$");
