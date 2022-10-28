3f1f44510c53f74cfea0f39484997dba051b7e39
rghangha <rghangha@asu.edu>
2022-10-27 23:26:59 -0700

    Input from urinal.dat and output to rule.txt was added

--- Urinals/urinals.java
+++ Urinals/urinals.java
@@ -1,14 +1,26 @@
 package Urinals;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class urinals {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         System.out.println("Author - Rahul Ghanghas");
+        System.out.println("Input string through keyboard :-");
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         System.out.println(goodString(s));
         System.out.println(countUrinals(s));
+        System.out.println("Input string through urinal.dat file :-");
+        File file = new File("C:\\Users\\rghangha\\UrinalsAssignment\\urinal.dat");
+        Writer wr = new FileWriter("C:\\Users\\rghangha\\UrinalsAssignment\\rule.txt");
+        Scanner sc2 = new Scanner(file);
+        while(sc2.hasNextLine()) {
+            String line = sc2.nextLine();
+            int freeUrinals = countUrinals(line);
+            wr.write(Integer.toString(freeUrinals)+"\n");
+        }
+        wr.close();
     }
     public static int countUrinals(String str){
         if (goodString(str)) {
