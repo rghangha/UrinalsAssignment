c9510d976b6d0d94f5ac4b976eed881ef515238c
rghangha <rghangha@asu.edu>
2022-10-27 22:31:50 -0700

    Input from keyboard part was added

--- Urinals/urinals.java
+++ Urinals/urinals.java
@@ -1,16 +1,22 @@
 package Urinals;
 
+import java.util.Scanner;
+
 public class urinals {
     public static void main(String[] args){
         System.out.println("Author - Rahul Ghanghas");
-        boolean validString = goodString("10001");
-        System.out.println(validString);
-        int freeUrinals = countUrinals("10001");
-        System.out.println(freeUrinals);
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(goodString(s));
+        System.out.println(countUrinals(s));
     }
     public static int countUrinals(String str){
-        return str.contains("11") ? -1 : str.replaceAll("010|10|01","1")
-                .replace("00","0").replace("1","").length();
+        if (goodString(str)) {
+            return str.contains("11") ? -1 : str.replaceAll("010|10|01", "1")
+                    .replace("00", "0").replace("1", "").length();
+        }
+        System.out.println("Input is not valid string, so Output would be -1");
+        return -1;
     }
     public static boolean goodString(String str){
         return str.matches("^[01]+$");
